e860d84bb2140a8818b0836e3784502e25397b97
Pedro <pedro.smith@unesp.br>
2024-11-26 02:27:01 -0300

    conclusoes_inisghts

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ df = sns.load_dataset('tips')
 # Título do app
 st.title("Análise de Gorjetas com Streamlit")
 
+# Objetivo
+st.write("""
+O objetivo principal desse aplicativo é explorar as relações entre gorjetas (tip), total da conta (total_bill), 
+sexo, dia da semana, horário e tamanho do grupo, oferecendo insights para otimizar estratégias 
+de atendimento em restaurantes. 
+""")
+
 # Dataset
+st.header("Leitura inicial dos dados")
 st.write(df)
 
 # Visão Geral
-st.header("Visão Geral do Dataset")
+st.header("Dados estatísticos gerais do Dataset")
 st.write(df.describe()) 
 
 # Seleção de variáveis
@@ -27,9 +35,20 @@ fig, ax = plt.subplots()
 sns.scatterplot(data=df, x=x_var, y=y_var, hue='sex', ax=ax)
 st.pyplot(fig)
 
-# Insights
+# Insights do grafico de dispersão
 st.header("Insights")
 st.write("""
-- Analisar os dias com maior volume de gorjetas.
-- Investigar se grupos maiores tendem a deixar gorjetas proporcionalmente menores.
+- Analisar os dias com maior volume de gorjetas (x=day, y=tip)
+    - Nessa análise, nota-se que aos finais de semana a quantidade e o valor das gorjetas tendem a ser mais altos, 
+    sendo majoritariamente pagas por homens (x=sex, y=tip)
+- Analisar os periodos em que existe uma maior taxa de gorjetas (y=tip, x=time)
+    - Nessa analise, nota-se que no periodo do jantar existe uma taxa bem maior de gorjetas, sendo normalmanete com um valor mais alto que no almoço
 """)
+
+# Conclusões do gráfico de dispersão
+st.header("Conclusão")
+st.write("""
+Conclui-se que seria interessante para o estabelecimento investir na qualidade do atendimento aos finais de semana ou até
+estender o horário de atendimento. Alèm disso, seria interessante propor algum incentivo para aumentar a presença de homens no estabelecimento
+""")
+
